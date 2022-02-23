0462f44917d9b3765b4439b65677a592ebc816e8
Nwamini Emmanuel Onyedikachi <emmanuelgodwin558@gmail.com>
2022-02-23 02:20:18 +0100

    added a crypto table

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -266,7 +266,13 @@
         <h1 class="liveprice-header"> cryptocurrently live price table
             <br><span class="line"></span>
         </h1>
-        <img src="{{asset('data/images/forex-trading-background_52683-41604.webp')}}" alt="">
+        <!-- this is the table that contains the updated prices  -->
+           <body>
+              <div style="height:560px; background-color: #000000; overflow:hidden; box-sizing: border-box; border-radius: 4px;
+                 border: 5px rgb(0, 0,0, 0.4) solid; text-align: right; line-height:14px; font-size: 12px; font-feature-settings: normal; text-size-adjust: 100%; padding:1px;padding: 0px; margin: 0px; width: 100%;"><div style="height:540px; padding:0px; margin:0px; width: 100%;"><iframe src="https://widget.coinlib.io/widget?type=chart&theme=light&coin_id=859&pref_coin_id=1505" width="100%" height="536px" scrolling="auto" marginwidth="0" marginheight="0" frameborder="0" border="0" style="border:0;margin:0;padding:0;line-height:14px;"></iframe></div></div>
+         </body>
+
+        {{-- <img src="{{asset('data/images/forex-trading-background_52683-41604.webp')}}" alt=""> --}}
         <!-- this content is just a demo  -->
     </section>
 
